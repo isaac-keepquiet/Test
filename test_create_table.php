b314485f27f3206a1b65f2d3f5801cb465758ba1
isaac-keepquiet <yang.isaac.jiao+github@gmail.com>
2021-11-19 18:32:07 +1100

    Modify test_create_table code

--- test_create_table.php
+++ test_create_table.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
 <?php
-// header('Content-Type: text/html; charset=utf-8');
 
 echo <<<'INPUT'
 <form>
@@ -33,4 +24,3 @@ for ($i=0;$i<$row;$i++){
 echo '</table>';
 ?>
 </body>
-</html>
